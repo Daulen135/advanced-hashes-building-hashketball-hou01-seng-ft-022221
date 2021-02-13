a6f2e46eaa1cf6aef277db6ccb316d686e752c68
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-13 02:54:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,7 +25,7 @@ hash_hash={
   
   :away =>{
     
-     :team_name =>"Brooklyn Nets",
+     :team_name =>"Charlotte Hornets",
     :colors =>["Black, White"],
     :players=>[
       {:player_name =>"Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12,:steals => 3,:blocks => 1,:slam_dunks =>1},    
