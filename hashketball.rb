2920354dcc6d55c52859d83f2f82f464ff1d4e5e
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-13 02:56:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,7 +28,7 @@ hash_hash={
      :team_name =>"Charlotte Hornets",
     :colors =>["Turquoise, Purple"],
     :players=>[
-      {:player_name =>"Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12,:steals => 3,:blocks => 1,:slam_dunks =>1},    
+      {:player_name =>"Jeff Adrien", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12,:steals => 3,:blocks => 1,:slam_dunks =>1},    
      
       {:player_name =>"Reggie Evans", :number => 30, :shoe =>14, :points =>12, :rebounds => 12, :assists => 12,:steals => 12,:blocks => 12,:slam_dunks =>7},    
       
